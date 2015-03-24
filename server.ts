ee2ef13d229f254a78211a1249cdc26dd703d298
anton <anton.v.gusarov@gmail.com>
2015-03-24 14:36:14 +0300

    insert

--- server.ts
+++ server.ts
@@ -7,23 +7,26 @@
 
 import mysql = require("mysql");
 import fs = require("fs");
-var data = require('zara_couts.json');
+var data = require('zara_treat.json');
 var mysqlLocal = {
     host     : '127.0.0.1',
     user     : 'root',
     password : '',
     database:  'clothes'
 },
-connection/* = mysql.createPool(mysqlLocal);
+connection = mysql.createPool(mysqlLocal);
+
 connection.on('connection', function(conn) {
 //    connection = conn;
     console.log('connected as id ' + conn.threadId);
-})*/;
-
-data.items.forEach((cloth, index)=>{
-    cloth.price = cloth.price.replace(/\s?руб\.$/i, '');
-    cloth.price = Number(cloth.price.replace(/\s/g, ''));
-    cloth.title = cloth.title.toLocaleLowerCase();
 });
-
-fs.writeFileSync('zara_treat.json', JSON.stringify(data), {encoding:'utf8'});
+var insert_prefix = 'INSERT INTO `Items` (title, link, price, image) VALUES',
+    insert_body = [];
+data.items.forEach((cloth, index)=>{
+    insert_body.push(" ('" + cloth.title + "', "
+    + connection.escape(cloth.link) + ", "
+    + cloth.price + ", "
+    + connection.escape(cloth.image) + ")");
+});
+//console.log(insert_prefix + insert_body);
+connection.query(insert_prefix + insert_body.join(', '), (err)=>{if (err) console.log(err)});
